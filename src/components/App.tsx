d2e780a10d92f7e2d20fa117c15bef11d7a484bb
Andrey Barchuk <andreyb@wizardsdev.com>
2020-02-18 18:11:33 +0200

    redux inprogress

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
+import store from '../redux';
 
-import PageInterface from '../PageInterface';
+// import PageInterface from '../interfaces/PageInterface';
 
-class App extends React.Component<PageInterface, {}> {
-    render() {
-        return (<div>
-                <h1>Welcome to React with Typescript АУЕ</h1>
-                <p>The color of this page is: {this.props.color}</p>
-            </div>
-        );
-    }
+class App extends React.Component {
+	render() {
+		return (
+			<Provider store={store}>
+				<div>
+					<h1>Welcome to React with Typescript АУЕ</h1>
+				</div>
+			</Provider>
+		);
+	}
 }
 
 export default App;
---
+++ src/redux/index.ts
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../rootReducers';
+import { composeEnhancers } from './utils';
+
+const middlewares = [];
+
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+
+const initialState = {};
+
+const store = createStore(rootReducer, initialState, enhancer);
+
+export default store;
