00945ee00d057969d92f5a157570b0f860ed1a32
Andrey Barchuk <mazafakand@gmail.com>
2020-03-02 01:53:58 +0200

    typescript inprogress

--- src/components/Counter/action.ts
+++ src/components/Counter/action.ts
@@ -1,6 +1,12 @@
-export default function addCount() {
+import { ActionTypes, ReduxBaseAction } from '../../rootReducers';
+
+export interface AddCountAction extends ReduxBaseAction {
+  type: ActionTypes.ADD_COUNT;
+}
+
+export default function addCount(): AddCountAction {
   console.log('addCount ACTION');
   return {
-    type: 'ADD_COUNT',
+    type: ActionTypes.ADD_COUNT,
   };
 }
--- src/rootReducers.ts
+++ src/rootReducers.ts
@@ -1,9 +1,18 @@
 import { combineReducers } from 'redux';
 import counter from './components/Counter/reducer';
 
-const rootReducer = () =>
-  combineReducers({
-    counter,
-  });
+export enum ActionTypes {
+  ADD_COUNT = 'ADD_COUNT',
+}
+
+export interface ReduxBaseAction {
+  type: ActionTypes;
+}
+
+const rootReducer = combineReducers({
+  counter,
+});
+
+export type AppState = ReturnType<typeof rootReducer>;
 
 export default rootReducer;
